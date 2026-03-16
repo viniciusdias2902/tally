1a8aac72ea679ced1c7d9f8b1a29dd1ce1955f92
Vinícius Dias <vyvy292004@gmail.com>
2026-03-16 19:06:47 -0300

    feat: adiciona app e server.js para inicialização do servidor express

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import prisma from "./lib/prisma.js";
+import { autenticar } from "./middlewares/autenticar.js";
+import { criarUsuarioRepositorio } from "./modules/auth/usuario.repositorio.js";
+import { criarAuthServico } from "./modules/auth/auth.servico.js";
+import { criarAuthController } from "./modules/auth/auth.controller.js";
+import { criarAuthRoutes } from "./modules/auth/auth.routes.js";
+
+const usuarioRepositorio = criarUsuarioRepositorio(prisma);
+const authServico = criarAuthServico(usuarioRepositorio);
+const authController = criarAuthController(authServico);
+const authRoutes = criarAuthRoutes(authController);
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use(autenticar);
+
+export default app;
