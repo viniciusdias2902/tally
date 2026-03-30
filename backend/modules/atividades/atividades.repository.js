bba6171bde6c9b3c618c8f5134374f47deb2178b
mariaritalustosa <mritalustosa.ad@gmail.com>
2026-03-30 13:51:34 -0300

    feat: add find activity by id method

--- backend/modules/atividades/atividades.repository.js
+++ backend/modules/atividades/atividades.repository.js
@@ -1,8 +1,13 @@
 export function criarAtividadeRepository(prisma) {
     return {
         criar({ nome, usuarioId }) {
-            return prisma.atividade.create({ data: { nome, usuarioId } })
+            return prisma.atividade.create({ data: { nome, usuarioId } });
         },
 
-    }
+        buscarPorId(id) {
+            return prisma.atividade.findUnique({ where: { id } });
+        },
+    };
 }
+
+
