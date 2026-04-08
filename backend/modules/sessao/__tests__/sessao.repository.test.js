8ffb03756d683a1cd061f5310d9fb1acbd286c42
mariaritalustosa <mritalustosa.ad@gmail.com>
2026-04-08 12:22:52 -0300

    test(sessao): adiciona teste de criação de sessão no repository

--- backend/modules/sessao/__tests__/sessao.repository.test.js
+++ backend/modules/sessao/__tests__/sessao.repository.test.js
@@ -22,3 +22,24 @@ describe("Sessao Repository", () => {
         prismaMock = criarPrismaMock();
         sessaoRepository = criarSessaoRepository(prismaMock);
     });
+
+    it("deve criar uma noca sessão", async () => {
+        const dadosSessao = {
+            atividadeId: 1,
+            categoriaId: 1,
+            iniciadoEm: new Date(),
+            duracaoSegundos: 3600,
+            modo: "foco",
+            ciclosPomodoro: 4,
+            observacoes: "Sessão de estudo",
+        };
+        const sessaoCriada = { ...dadosSessao, id: 1 };
+        prismaMock.sessao.create.mockResolvedValue(sessaoCriada);
+
+        const resultado = await sessaoRepository.criar(dadosSessao);
+
+        expect(prismaMock.sessao.create).toHaveBeenCalledWith({ data: dadosSessao });
+        expect(resultado).toEqual(sessaoCriada);
+
+    });
+});
