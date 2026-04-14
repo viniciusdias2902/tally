56245f2f4ab8fcce96012c63009ef3e84cea50c0
Vinícius Dias <vyvy292004@gmail.com>
2026-04-13 22:08:59 -0300

    style(sidebar): remover borda direita entre sidebar e conteúdo

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -21,7 +21,7 @@ export default function Sidebar({ aberto, onFechar }) {
       )}
 
       <aside
-        className={`fixed top-0 left-0 z-50 h-full w-64 bg-bg-elevated border-r border-border flex flex-col transition-transform lg:translate-x-0 lg:static lg:z-auto ${
+        className={`fixed top-0 left-0 z-50 h-full w-64 bg-bg-elevated flex flex-col transition-transform lg:translate-x-0 lg:static lg:z-auto ${
           aberto ? "translate-x-0" : "-translate-x-full"
         }`}
       >
